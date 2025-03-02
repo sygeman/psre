1705782f01d0cd5feb1e860514789db590d4dda4
Sygeman <sygeman@gmail.com>
2025-03-03 01:46:16 +0300

    feat: add emoji icons for all resources

--- src/modules/resources-panel.tsx
+++ src/modules/resources-panel.tsx
@@ -11,20 +11,20 @@ export const ResourcesPanel = () => {
 
   return (
     <div class="grid px-4 w-full h-8 grid-cols-5">
-      <div class="flex items-center">
-        F {formatter.format(accountState.food)}
+      <div class="flex items-center gap-1">
+        🌾 {formatter.format(accountState.food)}
       </div>
-      <div class="flex items-center">
-        W {formatter.format(accountState.wood)}
+      <div class="flex items-center gap-1">
+        🪵 {formatter.format(accountState.wood)}
       </div>
-      <div class="flex items-center">
-        S {formatter.format(accountState.steel)}
+      <div class="flex items-center gap-1">
+        🔩 {formatter.format(accountState.steel)}
       </div>
-      <div class="flex items-center">
-        F {formatter.format(accountState.fuel)}
+      <div class="flex items-center gap-1">
+        🛢️ {formatter.format(accountState.fuel)}
       </div>
       <div 
-        class="flex items-center cursor-pointer hover:text-yellow-400 transition-colors"
+        class="flex items-center gap-1 cursor-pointer hover:text-yellow-400 transition-colors"
         onClick={() => navigate('/shop')}
       >
         💎 {formatter.format(accountState.diamond)}
