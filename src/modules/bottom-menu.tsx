914be01ba2c6fd9e37f2f435b2cdb465ca9ba5b6
Sygeman <sygeman@gmail.com>
2025-03-04 00:25:38 +0300

    feat: добавлены иконки в нижнее меню

--- src/modules/bottom-menu.tsx
+++ src/modules/bottom-menu.tsx
@@ -1,23 +1,28 @@
 import { A, useLocation } from '@solidjs/router';
 import { createMemo } from 'solid-js';
+import { Icon } from 'solid-heroicons';
+import { home, globeAlt, trophy, userCircle } from 'solid-heroicons/outline';
 
 export const BottomMenu = () => {
   const location = useLocation();
-  const home = createMemo(() => location.pathname === '/');
+  const isHome = createMemo(() => location.pathname === '/');
 
   return (
     <div class="grid h-16 w-full grid-cols-3 bg-slate-800">
       <A
-        href={home() ? '/region' : '/'}
-        class="flex items-center justify-center"
+        href={isHome() ? '/region' : '/'}
+        class="flex flex-col items-center justify-center gap-1"
       >
-        {home() ? 'Мир' : 'Домой'}
+        <Icon path={isHome() ? globeAlt : home} class="h-6 w-6" />
+        <div class="text-xs">{isHome() ? 'Мир' : 'Домой'}</div>
       </A>
-      <A href="/heroes" class="flex items-center justify-center">
-        Герои
+      <A href="/heroes" class="flex flex-col items-center justify-center gap-1">
+        <Icon path={userCircle} class="h-6 w-6" />
+        <div class="text-xs">Герои</div>
       </A>
-      <A href="/rank" class="flex items-center justify-center">
-        Ранги
+      <A href="/rank" class="flex flex-col items-center justify-center gap-1">
+        <Icon path={trophy} class="h-6 w-6" />
+        <div class="text-xs">Ранги</div>
       </A>
     </div>
   );
