2f5053adb4d3d753e175285da887aaf898f877eb
Sygeman <sygeman@gmail.com>
2025-03-04 00:47:27 +0300

    chore: добавлена конфигурация text-shadow в tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      textShadow: {
+        sm: '0 0 2px var(--tw-shadow-color)',
+      },
+    },
+  },
+  plugins: [
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      );
+    },
+  ],
+}; 
