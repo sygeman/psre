c4c8a6b602f2f3098d50837638ed4ecdf011b112
Sygeman <sygeman@gmail.com>
2025-03-05 23:31:58 +0300

    upd server

--- server.ts
+++ server.ts
@@ -8,37 +8,39 @@ function getRandomInt(n: number) {
     return Math.floor(Math.random() * n) + 1;
   }
 
-// const stateId = '5fd7059b-fb52-40e9-92b0-2ddbd4b2d2f6';
+const stateId = '5fd7059b-fb52-40e9-92b0-2ddbd4b2d2f6';
 
-// setInterval(async () => {
-//     await directus.request(updateItem('psre_account_state', stateId, {
-//         action_points: getRandomInt(9999),
-//         stamina_points: getRandomInt(9999),
-//         level: getRandomInt(100),
-//         food: getRandomInt(10000000),
-//         wood: getRandomInt(10000000),
-//         steel: getRandomInt(10000000),
-//         fuel: getRandomInt(10000000),
-//         diamond: getRandomInt(100000),
-//         power: getRandomInt(1000000),
-//         serum: getRandomInt(1000000),
-//     }))
-// }, 3000)
+setInterval(async () => {
+    await directus.request(updateItem('psre_account_state', stateId, {
+        action_points: getRandomInt(120),
+        stamina_points: getRandomInt(120),
+        level: getRandomInt(100),
+        food: getRandomInt(10000000),
+        wood: getRandomInt(10000000),
+        steel: getRandomInt(10000000),
+        fuel: getRandomInt(10000000),
+        diamond: getRandomInt(100000),
+        power: getRandomInt(1000000),
+        serum: getRandomInt(1000000),
+    }))
+}, 3000)
 
 const regionChatId = 'def67dc7-39ca-4f13-a816-6f53c115d869'
 const allianceChatId = '52b49c7a-5b8a-40b9-95af-c31be0c8e39b'
 
 const accountId = 'ba12e291-2e9c-452e-ae06-81c1a885390e';
 
-await directus.request(deleteItems('psre_chat_message', {
-  filter: { chat_id: { _eq: regionChatId }, },
-  limit: -1
-}))
-
-await directus.request(deleteItems('psre_chat_message', {
-  filter: { chat_id: { _eq: allianceChatId } },
-  limit: -1
-}))
+setInterval(async () => {
+  await directus.request(deleteItems('psre_chat_message', {
+    filter: { chat_id: { _eq: regionChatId }, },
+    limit: -1
+  }))
+  
+  await directus.request(deleteItems('psre_chat_message', {
+    filter: { chat_id: { _eq: allianceChatId } },
+    limit: -1
+  }))
+}, 20000)
 
 setInterval(async () => {
   await directus.request(createItem('psre_chat_message', {
