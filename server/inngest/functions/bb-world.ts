dd113d0ee2e1f837f872a2af45dedcac84542f2f
Sygeman <sygeman@gmail.com>
2025-05-31 02:27:53 +0300

    feat: добавлены новые тестовые функции и утилиты - добавлены test-random-chat-message и test-random-state функции, создана папка mocks, добавлена утилита random-int

--- server/inngest/functions/bb-world.ts
+++
@@ -1,9 +0,0 @@
-import { inngest } from "../client";
-
-export default inngest.createFunction(
-    { id: 'bb-world' },
-    { event: 'bb-world'},
-    async ({ event }) => {
-      console.log('Function called', event);
-    }
-  );
---
+++ server/inngest/functions/test-random-chat-message.ts
@@ -0,0 +1,29 @@
+import { createItem } from "@directus/sdk";
+import { directus } from "../../../src/lib/directus";
+import { inngest } from "../client";
+import { CronTime } from "cron-time-generator";
+import { allianceMessages, regionMessages } from "../mocks/messages";
+
+export default inngest.createFunction(
+    { id: "test-random-chat-message" },
+    { cron: `TZ=Europe/Moscow ${CronTime.everyMinute()}` },
+    async () => {
+
+      const accountId = 'ba12e291-2e9c-452e-ae06-81c1a885390e';
+      const regionChatId = 'def67dc7-39ca-4f13-a816-6f53c115d869'
+      const allianceChatId = '52b49c7a-5b8a-40b9-95af-c31be0c8e39b'
+
+      await directus.request(createItem('psre_chat_message', {
+          chat_id: regionChatId,
+          content: regionMessages[Math.floor(Math.random() * regionMessages.length)],
+          author: accountId
+      }));
+
+      await directus.request(createItem('psre_chat_message', {
+        chat_id: allianceChatId,
+        content: allianceMessages[Math.floor(Math.random() * allianceMessages.length)],
+        author: accountId
+      }));
+
+    }
+  );
