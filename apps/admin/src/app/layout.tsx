6afc43369033b89ca457878bf48c9e9969f27604
Sygeman <sygeman@gmail.com>
2025-05-31 15:19:41 +0300

    feat: enable dark theme by default and clean up admin page

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ru" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
