d56a43069f86b3e07116d8eb47b9c0381078b3de
Sygeman <sygeman@gmail.com>
2025-06-05 18:19:10 +0300

    Обновил apollo настройки и добавил Tailwind CSS

--- apps/vue-app/src/apollo.ts
+++ apps/vue-app/src/apollo.ts
@@ -5,17 +5,7 @@ import { createClient } from 'graphql-ws'
 // WebSocket подключение для всех операций (queries, mutations, subscriptions)
 const wsLink = new GraphQLWsLink(
   createClient({
-    url: 'ws://localhost:5000/graphql',
-    //   connectionParams: () => {
-    //     // Note: getSession() is a placeholder function created by you
-    //     const session = getSession();
-    //     if (!session) {
-    //       return {};
-    //     }
-    //     return {
-    //       Authorization: `Bearer ${session.token}`,
-    //     };
-    //   },
+    url: 'http://localhost:5000/graphql',
   }),
 )
 
