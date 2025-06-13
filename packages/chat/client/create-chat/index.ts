81cc92bba30cbec57c6811c669944d72a6490de9
Sygeman <sygeman@gmail.com>
2025-06-14 02:00:48 +0300

    upd chat

--- packages/chat/client/create-chat/index.ts
+++ packages/chat/client/create-chat/index.ts
@@ -1,57 +1,23 @@
-import { For, createEffect, createSignal, onCleanup } from "solid-js";
-import { createMutation, gql, useApollo } from "@psre/apollo";
+import { createEffect, createSignal, onCleanup } from "solid-js";
+import { createMutation, useApollo } from "@psre/apollo";
 import {
   ChatCleanupSubscription,
   ChatCleanupSubscriptionVariables,
   CreateMessageMutation,
   CreateMessageMutationVariables,
+  GetChatsQuery,
+  GetChatsQueryVariables,
   GetNewChatMessagesSubscription,
   GetNewChatMessagesSubscriptionVariables,
 } from "./create-chat.gql.types";
 import { ChatMessage } from "@psre/types";
-import { chatStore } from "./store";
-
-const CHAT_MESSAGE_FRAGMENT = gql`
-  fragment ChatMessageFragment on ChatMessage {
-    id
-    content
-    author {
-      id
-      name
-    }
-    date_created
-  }
-`;
-
-const CHATS_QUERY = gql`
-  query GetChats {
-    chats {
-      id
-      type
-      messages {
-        ...ChatMessageFragment
-      }
-    }
-  }
-
-  ${CHAT_MESSAGE_FRAGMENT}
-`;
-
-const CHAT_NEW_MESSAGE_SUBSCRIPTION = gql`
-  subscription GetNewChatMessages($chatId: String!) {
-    createdChatMessage(chatId: $chatId) {
-      ...ChatMessageFragment
-    }
-  }
-
-  ${CHAT_MESSAGE_FRAGMENT}
-`;
-
-const CHAT_CLEANUP_SUBSCRIPTION = gql`
-  subscription ChatCleanup($chatId: String!) {
-    chatCleanup(chatId: $chatId)
-  }
-`;
+import { chatStore } from "../store";
+import {
+  CHAT_CLEANUP_SUBSCRIPTION,
+  CHAT_NEW_MESSAGE_SUBSCRIPTION,
+  CHATS_QUERY,
+  CREATE_MESSAGE_MUTATION,
+} from "./gql";
 
 export const createChat = () => {
   const apolloClient = useApollo();
@@ -59,25 +25,38 @@ export const createChat = () => {
   const [chatId, setChatId] = createSignal<string | null>(null);
 
   const updateChatHistory = () => {
-    apolloClient.query({ query: CHATS_QUERY }).then((data) => {
-      const chat = data?.data.chats.find(
-        (chat) => chat.type === chatStore.activeChannel,
-      );
-      setChatId(chat.id);
-      setMessages(chat.messages);
+    apolloClient
+      .query<GetChatsQuery, GetChatsQueryVariables>({ query: CHATS_QUERY })
+      .then((data) => {
+        const chat = data?.data.chats.find(
+          (chat) => chat.type === chatStore.activeChannel,
+        );
+        setChatId(chat.id);
+        setMessages(chat.messages);
+      });
+  };
+
+  const [createMessageMutation] = createMutation<
+    CreateMessageMutation,
+    CreateMessageMutationVariables
+  >(CREATE_MESSAGE_MUTATION);
+
+  const createMessage = (content: string) => {
+    if (typeof chatId() !== "string") return;
+    createMessageMutation({
+      variables: {
+        input: { chatId: chatId(), content },
+      },
     });
   };
 
-  const [createMessage] = createMutation<
-    CreateMessageMutation,
-    CreateMessageMutationVariables
-  >(gql`
-    mutation CreateMessage($input: SendMessageInput!) {
-      createChatMessage(input: $input)
-    }
-  `);
-
   createEffect(() => {
+    console.log("effect", chatId());
+    const _ = chatId();
+    if (typeof chatId() !== "string") return;
+
+    console.log("do subs");
+
     const newMessageSubscription = apolloClient
       .subscribe<
         GetNewChatMessagesSubscription,
@@ -87,7 +66,6 @@ export const createChat = () => {
         variables: { chatId: chatId() },
       })
       .subscribe({
-        // error: reject,
         next: ({ data }) => {
           const newMessage = data?.createdChatMessage;
           setMessages((messages) => [...messages, newMessage]);
@@ -100,7 +78,6 @@ export const createChat = () => {
         variables: { chatId: chatId() },
       })
       .subscribe({
-        // error: reject,
         next: () => {
           setMessages([]);
         },
@@ -109,24 +86,20 @@ export const createChat = () => {
     updateChatHistory();
 
     onCleanup(() => {
+      console.log("cleanup");
       setMessages([]);
       newMessageSubscription.unsubscribe();
       cleanupSubscription.unsubscribe();
     });
   });
 
+  createEffect(() => {
+    const _ = chatStore.activeChannel;
+    updateChatHistory();
+  });
+
   return {
     messages,
-    createMessage: (content: string) => {
-      if (typeof chatId() !== "string") return;
-      createMessage({
-        variables: {
-          input: {
-            chatId: chatId(),
-            content,
-          },
-        },
-      });
-    },
+    createMessage,
   };
 };
